c1ac047201af5012658a88380e12b7f65e36a319
Mike Simons <simonsm2@gmail.com>
2020-12-12 15:55:00 -0500

    Day ten part one

--- Sources/DayTen/main.swift
+++ Sources/DayTen/main.swift
@@ -2,43 +2,69 @@
 //  File.swift
 //  
 //
-//  Created by Michael Simons on 12/6/20.
+//  Created by Michael Simons on 12/10/20.
 //
 
 import Foundation
 
 
+var sorted = [0]
+sorted.append(contentsOf: input.sorted())
 
 
-struct CustomsForm {
-    let count: Int
-    init(_ string: String) {
-        let entries = string.components(separatedBy: .newlines)
-        var characterSet = Set<Character>()
-        
-        for entry in entries {
-            characterSet.formUnion(Set(entry))
-        }
-        
-        count = characterSet.count
+print(" first - \(sorted.first ?? 0)")
+
+var ones = 0
+var threes = 0
+
+let differences: [Int] = sorted.enumerated().map { (index, adaptor) in
+    guard index < sorted.count - 1 else {
+        threes += 1
+        return 3
+    }
+    let next = sorted[index + 1]
+    let current = adaptor
+    let difference = next - current
+    
+    if difference == 1 {
+        ones += 1
+    } else if difference == 3 {
+        threes += 1
     }
     
-    init(partTwo string: String) {
-        let entries = string.components(separatedBy: .newlines)
-        var characterSet = Set<Character>(entries.first!)
-        
-        for entry in entries {
-            characterSet.formIntersection(Set(entry))
-        }
-        
-        count = characterSet.count
+    return sorted[index + 1] - adaptor
+}
+
+print("\(ones) \(threes) \(ones * threes)" )
+
+struct Adaptor {
+    let value: Int
+    let children: [Int]
+}
+
+let inRange: [Int] = sorted.enumerated().map { (index, adaptor) in
+    guard index < sorted.count - 1 else {
+        return 1
     }
+    
+    var count = 0
+    
+    var endIndex: Int = index
+    
+    let _ = sorted.formIndex(&endIndex, offsetBy: 3, limitedBy: sorted.endIndex)
+    
+    let next = sorted[index..<endIndex]
+    
+    
+    for n in next {
+        if adaptor - n <= 3 {
+            count += 1
+        }
+    }
+    
+    return count
 }
 
 
-let total = input.components(separatedBy: "\n\n").map { CustomsForm($0).count }.reduce(0, +)
-
-print("Part 1: \(total)")
-let total2 = input.components(separatedBy: "\n\n").map { CustomsForm(partTwo: $0).count }.reduce(0, +)
-
-print("Part 2: \(total2)")
+print("\(inRange.count)")
+//print("\(inRange.reduce(1, *))")
